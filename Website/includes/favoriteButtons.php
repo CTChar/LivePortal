b61efc68514718771de3e81ad239512c0a5082e9
sburlington6 <sburlington6@yahoo.com>
2015-03-20 10:34:47 -0400

    favorites
    fixed favorite buttons when not logged in

--- Website/includes/favoriteButtons.php
+++ Website/includes/favoriteButtons.php
@@ -2,26 +2,27 @@
 
 
 	$action = basename($_SERVER['PHP_SELF']);
-
-if (!favorited($_SESSION['userId'],$favorited))
+if (isLoggedIn())
 {
-?>
-	<form action="<?php echo $action; ?>">
-		<input type="hidden" name="userId" value="<?php echo $favorited?>">
-		<input type="submit" name="favoriteSubmit" value="Favorite">
-	</form>
+	if (!favorited($_SESSION['userId'],$favorited))
+	{
+	?>
+		<form action="<?php echo $action; ?>">
+			<input type="hidden" name="userId" value="<?php echo $favorited?>">
+			<input type="submit" name="favoriteSubmit" value="Favorite">
+		</form>
 
-<?php
+	<?php
+	}
+	elseif ($_SESSION['userId'] != $favorited)
+	{
+	?>
+		<form action="<?php echo $action; ?>">
+			<input type="hidden" name="userId" value="<?php echo $favorited?>">
+			<input type="submit" name="unFavoriteSubmit" value="Un-favorite">
+		</form>
+	<?php
+	}
 }
-elseif ($_SESSION['userId'] != $favorited)
-{
-?>
-	<form action="<?php echo $action; ?>">
-		<input type="hidden" name="userId" value="<?php echo $favorited?>">
-		<input type="submit" name="unFavoriteSubmit" value="Un-favorite">
-	</form>
-<?php
-}
-
 echo ('<br/>');
 ?>
