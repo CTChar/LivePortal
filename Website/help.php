0dc06c8efa357aa95bf06fad00c271268d7b4b94
Davidcd <decdavid@ymail.com>
2015-04-10 11:09:18 -0400

    Added content to the help page.

--- Website/help.php
+++ Website/help.php
@@ -1,14 +1,14 @@
 <?php
 require_once('includes/functions.php');
-
-
 require_once('includes/header.php');
 
 ?>
 <div class="jumbotron">
 	<h1>Help</h1> 
 </div>
-	
+<div id="content">
+	For help, email us at fakeSupportEmail@LivePortal.gq 
+</div>
 <?php
 require_once('includes/footer.php')
 
