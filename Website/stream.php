911d42ee26cdebd44ea62f2875929451e1d9b1ce
Richard Bird <sburlington6@yahoo.com>
2015-04-24 13:12:04 -0400

    Stream page
    added user image to stream
    put irc next to stream instead of below
    bootstrap moves it below on small screens

--- Website/stream.php
+++ Website/stream.php
@@ -21,23 +21,27 @@ $key = getFromTable ('Accounts', 'accountId', clean($userId), 'streamKey');
 			?>
 		</h1> 
 	</div>
+<div class="row">
 
-	<script src="http://jwpsrv.com/library/Djeg0sQVEeSFjg4AfQhyIQ.js"></script>
-
-	<div id="player">Loading the player...</div>
-	<script type="text/javascript">
-	  jwplayer("player").setup({
-	  file: "rtmp://server.liveportal.gq/liveportal/<?php echo("$key");?>",
-	  image: "http://example.com/uploads/myPoster.jpg",
-	  width: 900,
-	  height: 500
-	  });
-	</script>
-
-	<div class="irc">
-		<iframe src="https://kiwiirc.com/client/server.liveportal.gq/?nick=Test|?&theme=cli#<?php echo $username; ?>" style="border:none; width:900px; height:500px; margin-left: 40px"></iframe>
+	<div class="col-md-6">
+		<script src="http://jwpsrv.com/library/Djeg0sQVEeSFjg4AfQhyIQ.js"></script>
+		<div id="player">Loading the player...</div>
+		<script type="text/javascript">
+		  jwplayer("player").setup({
+		  file: "rtmp://server.liveportal.gq/liveportal/<?php echo("$key");?>",
+		  image: "<?php echo getAvatar($username,500) ?>",
+		  //width: 450,
+		  height: 500
+		  });
+		</script>
 	</div>
-
+	
+	<div class="col-md-6">
+		<div class="irc">
+			<iframe src="https://kiwiirc.com/client/server.liveportal.gq/?nick=Test|?&theme=cli#<?php echo $username; ?>" style="border:none; width:470px; height:500px; margin: 0px"></iframe>
+		</div>
+	</div>
+</div>
 
 <?php
 require_once('includes/footer.php')
